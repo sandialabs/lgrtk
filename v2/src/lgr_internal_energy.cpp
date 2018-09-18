b2d5d70a4410a0cdd3a2de2535eb19617fa1b006
Dan Ibanez <daibane@sandia.gov>
2018-09-18 14:27:04 -0600

    ensure acceleration is zero, rename object

--- v2/src/lgr_internal_energy.cpp
+++ v2/src/lgr_internal_energy.cpp
@@ -17,6 +17,7 @@ struct InternalEnergy : public Model<Elem> {
       this->point_define("e_dot", "specific internal energy rate", 1,
           RemapType::PER_UNIT_VOLUME, "");
     this->sim.fields[this->sim.stress].default_value = "symm(0.0)";
+    this->sim.fields[this->sim.acceleration].default_value = "vector(0.0)";
   }
   ModelOrder order() override final { return IS_FIELD_UPDATE; }
   char const* name() override final { return "internal energy"; }
