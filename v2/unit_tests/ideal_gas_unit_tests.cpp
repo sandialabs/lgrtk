5d99218a8da982c6079106fdd2b314ff333daea1
Dan Ibanez <daibane@sandia.gov>
2018-09-25 09:15:53 -0600

    Test non-zero energy for ideal gas
    there is now a safety check against
    zero energy

--- v2/unit_tests/ideal_gas_unit_tests.cpp
+++ v2/unit_tests/ideal_gas_unit_tests.cpp
@@ -1,14 +1,14 @@
 #include <lgr_ideal_gas.hpp>
 #include "lgr_gtest.hpp"
 
-TEST(ideal_gas, zero_energy) {
+TEST(ideal_gas, unit_inputs) {
   auto gamma = 1.4;
   auto rho = 1.0;
-  auto e = 0.0;
+  auto e = 1.0;
   double p, c;
   lgr::ideal_gas_update(gamma, rho, e, p, c);
-  EXPECT_TRUE(Omega_h::are_close(p, 0.0));
-  EXPECT_TRUE(Omega_h::are_close(c, 0.0));
+  EXPECT_TRUE(Omega_h::are_close(p, 0.4));
+  EXPECT_TRUE(Omega_h::are_close(c, std::sqrt(1.4 * 0.4)));
 }
 
 ALEXA_END_TESTS
