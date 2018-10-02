1a7bf6072fb55cee1c2e2c893e1db0cb2a46b956
Dan Ibanez <daibane@sandia.gov>
2018-10-02 16:29:09 -0600

    update and clean up Mie-Gruniesen

--- v2/src/lgr_mie_gruneisen.hpp
+++ v2/src/lgr_mie_gruneisen.hpp
@@ -4,84 +4,20 @@
 #include <lgr_element_types.hpp>
 #include <lgr_model.hpp>
 #include <string>
-#ifndef OMEGA_H_THROW
-#include <exception>
-#endif
 
 namespace lgr {
 
-namespace mie_gruneisen_details {
-
-OMEGA_H_INLINE
-void
-read_and_validate_params(Teuchos::ParameterList& pl,
-    double& rho0, double& gamma0, double& c0, double& s1, double& e0)
-{
-  auto errors = 0;
-  auto os = std::ostringstream();
-
-  // Read and validate inputs
-  if (!pl.isParameter("rho0")) {
-    errors++;
-    os << "Mie Gruneisen model requires 'initial density' parameter\n";
-  }
-  else {
-    rho0 = pl.get<double>("rho0");
-  }
-
-  if (!pl.isParameter("gamma0")) {
-    errors++;
-    os << "Mie Gruneisen model requires 'gamma0' parameter\n";
-  }
-  else {
-    gamma0 = pl.get<double>("gamma0");
-  }
-
-  if (!pl.isParameter("c0")) {
-    errors++;
-    os << "Mie Gruneisen model requires 'c0' parameter\n";
-  }
-  else {
-    c0 = pl.get<double>("c0");
-  }
-
-  if (!pl.isParameter("s1")) {
-    errors++;
-    os << "Mie Gruneisen model requires 's1' parameter\n";
-  }
-  else {
-    s1 = pl.get<double>("s1");
-  }
-
-  if (!pl.isParameter("e0")) {
-    errors++;
-    os << "Mie Gruneisen model requires 'e0' parameter\n";
-  }
-  else {
-    e0 = pl.get<double>("e0");
-  }
-
-  if (errors != 0) {
-#ifdef OMEGA_H_THROW
-    throw Omega_h::exception(os.str());
-#else
-    throw std::invalid_argument(os.str());
-#endif
-  }
-}
-} // mie_gruneisen_details
-
 //
-// This implementation of Mie Gruneisen is based on the hugoniot
+// This implementation of Mie Gruneisen is based on the Hugoniot
 // relations. In the code, you'll see references to 'ph' and 'eh'
-// which are the  pressure and energy on the hugoniot, respectively.
+// which are the  pressure and energy on the Hugoniot, respectively.
 //
-// The value of 'ph' the hugoniot pressure is found by assuming a
+// The value of 'ph' the Hugoniot pressure is found by assuming a
 // linear us-up relation:
 //
 //                     u_s = c_0 + s * u_p
 //
-// Then, by using the hugoniot equations for the conseration of mass
+// Then, by using the Hugoniot equations for the conseration of mass
 // and momentum we get to:
 //
 //                             C_0^2
@@ -143,36 +79,34 @@ read_and_validate_params(Teuchos::ParameterList& pl,
 
 
 OMEGA_H_INLINE void mie_gruneisen_update(
-    double rho0, double gamma0, double c0, double s1,
-    double rho, double internal_energy,
-    double& pressure, double& wave_speed)
+    double const rho0,
+    double const gamma0,
+    double const c0,
+    double const s1,
+    double const rho,
+    double const internal_energy,
+    double& pressure,
+    double& wave_speed)
 {
-
-  const double mu = 1.0 - (rho0 / rho);
-  const double dmu = rho0 / rho / rho; /* = \frac{\partial \mu}{\partial \rho} */
-
+  auto const mu = 1.0 - (rho0 / rho);
+  auto const dmu = rho0 / rho / rho; /* = \frac{\partial \mu}{\partial \rho} */
   // For expansion (mu <= 0):
-  const bool compression = (mu > 0.);
-
+  auto const compression = (mu > 0.);
   //  * limit 'us' to not drop below 'c0'
-  const double us = c0 / (1.0 - s1 * Omega_h::max2(0.0, mu));
-  const double ph = rho0 * us * us * mu;
-  const double eh = 0.5 * ph * mu / rho0;
-
+  auto const us = c0 / (1.0 - s1 * Omega_h::max2(0.0, mu));
+  auto const ph = rho0 * us * us * mu;
+  auto const eh = 0.5 * ph * mu / rho0;
   // derivative of pressure with respect to density
-  const double dus = compression ? (us * s1 / (1.0 - s1 * mu)) * dmu : 0.0;
-  const double dph = 2.0 * rho0 * us * dus * mu + rho0 * us * us * dmu;
-  const double deh = 0.5 * (mu * dph + ph * dmu) / rho0;
-  const double dpdrho = dph - gamma0 * rho0 * deh;
-
+  auto const dus = compression ? (us * s1 / (1.0 - s1 * mu)) * dmu : 0.0;
+  auto const dph = 2.0 * rho0 * us * dus * mu + rho0 * us * us * dmu;
+  auto const deh = 0.5 * (mu * dph + ph * dmu) / rho0;
+  auto const dpdrho = dph - gamma0 * rho0 * deh;
   //derivative of pressure with repect to energy
-  const double dpde = gamma0 * rho0;
-
+  auto const dpde = gamma0 * rho0;
   // Pressure
   pressure = ph + gamma0 * rho0 * (internal_energy - eh);
-
   // Wave speed
-  const double bulk_modulus = rho * dpdrho + (pressure / rho) * dpde;
+  auto const bulk_modulus = rho * dpdrho + (pressure / rho) * dpde;
   wave_speed = std::sqrt(bulk_modulus / rho);
 }
 
