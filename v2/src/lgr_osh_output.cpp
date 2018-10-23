f8c3e39b8355a0bfab63c9df89e3c5c7484fe378
Dan Ibanez <daibane@sandia.gov>
2018-10-23 16:20:07 -0600

    Draft osh output response
    the main purpose is
    checkpoint-restart,
    but so far untested

---
+++ v2/src/lgr_osh_output.cpp
@@ -0,0 +1,38 @@
+#include <lgr_osh_output.hpp>
+#include <Omega_h_file.hpp>
+#include <lgr_field_index.hpp>
+#include <lgr_response.hpp>
+#include <lgr_simulation.hpp>
+
+namespace lgr {
+
+struct OshOutput : public Response {
+  std::vector<FieldIndex> field_indices;
+  std::string prefix;
+  OshOutput(Simulation& sim_in, Omega_h::InputMap& pl)
+    :Response(sim_in, pl)
+    ,prefix(pl.get<std::string>("prefix", "checkpoint_"))
+  {
+    for (auto& field_ptr : sim.fields.storage) {
+      if (field_ptr->remap_type != RemapType::NONE) {
+        field_indices.push_back(sim.fields.find(field_ptr->long_name));
+      }
+    }
+  }
+  void out_of_line_virtual_method() override;
+  void respond() override final {
+    sim.disc.mesh.set_coords(sim.get(sim.position)); // linear specific!
+    sim.fields.copy_to_omega_h(sim.disc, field_indices);
+    auto path = prefix + std::to_string(sim.step) + ".osh";
+    Omega_h::binary::write(path, &sim.disc.mesh);
+    sim.fields.remove_from_omega_h(sim.disc, field_indices);
+  }
+};
+
+void OshOutput::out_of_line_virtual_method() {}
+
+Response* osh_output_factory(Simulation& sim, std::string const&, Omega_h::InputMap& pl) {
+  return new OshOutput(sim, pl);
+}
+
+}
