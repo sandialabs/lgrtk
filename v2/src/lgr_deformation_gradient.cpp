cfd796c7ae380740efa01a0784eacbbd93b6c5f8
Dan Ibanez <daibane@sandia.gov>
2018-11-05 15:52:21 -0700

    syntactic modernization of deformation gradient

--- v2/src/lgr_deformation_gradient.cpp
+++ v2/src/lgr_deformation_gradient.cpp
@@ -17,28 +17,29 @@ struct DeformationGradient : public Model<Elem> {
   std::uint64_t exec_stages() override final { return AT_FIELD_UPDATE; }
   char const* name() override final { return "deformation gradient"; }
   void at_field_update() override final {
-    auto points_to_grad = this->points_get(this->sim.gradient);
-    auto points_to_F = this->points_getset(this->deformation_gradient);
-    auto elems_to_nodes = this->get_elems_to_nodes();
-    auto nodes_to_v = this->sim.get(this->sim.velocity);
-    auto dt = this->sim.dt;
+    auto const points_to_grad = this->points_get(this->sim.gradient);
+    auto const points_to_F = this->points_getset(this->deformation_gradient);
+    auto const elems_to_nodes = this->get_elems_to_nodes();
+    auto const nodes_to_v = this->sim.get(this->sim.velocity);
+    auto const dt = this->sim.dt;
     auto functor = OMEGA_H_LAMBDA(int point) {
-      auto elem = point / Elem::points;
-      auto elem_nodes = getnodes<Elem>(elems_to_nodes, elem);
-      auto v = getvecs<Elem>(nodes_to_v, elem_nodes);
-      auto dN_dxnp1 = getgrads<Elem>(points_to_grad, point);
-      auto dv_dxnp1 = grad<Elem>(dN_dxnp1, v);
-      auto I = identity_matrix<Elem::dim, Elem::dim>();
-      auto dxn_dxnp1 = I - dt * dv_dxnp1;
-      auto dxnp1_dxn = invert(dxn_dxnp1);
-      auto dxn_dX = getfull<Elem>(points_to_F, point);
+      auto const elem = point / Elem::points;
+      auto const elem_nodes = getnodes<Elem>(elems_to_nodes, elem);
+      auto const v = getvecs<Elem>(nodes_to_v, elem_nodes);
+      auto const dN_dxnp1 = getgrads<Elem>(points_to_grad, point);
+      auto const dv_dxnp1 = grad<Elem>(dN_dxnp1, v);
+      auto const I = identity_matrix<Elem::dim, Elem::dim>();
+      auto const dxn_dxnp1 = I - dt * dv_dxnp1;
+      auto const dxnp1_dxn = invert(dxn_dxnp1);
+      auto const dxn_dX = getfull<Elem>(points_to_F, point);
       OMEGA_H_CHECK(determinant(dxn_dX) > 0.0);
-      OMEGA_H_CHECK(determinant(dxnp1_dxn) > 0.0);
-      auto dxnp1_dX = dxn_dX * dxnp1_dxn;
+      auto const det_dxnp1_dxn = determinant(dxnp1_dxn);
+      OMEGA_H_CHECK(det_dxnp1_dxn > 0.0);
+      auto const dxnp1_dX = dxn_dX * dxnp1_dxn;
       OMEGA_H_CHECK(determinant(dxnp1_dX) > 0.0);
       setfull<Elem>(points_to_F, point, dxnp1_dX);
     };
-    parallel_for("deformation gradient kernel", this->points(), std::move(functor));
+    parallel_for(this->points(), std::move(functor));
   }
 };
 
