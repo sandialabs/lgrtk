7a1f8c9d8ea8da631518450933bf8567506837d3
Brian Granzow <bngranz@sandia.gov>
2018-11-06 13:04:10 -0700

    Generalize to ents2nodes, nodes2ents in quadratic

--- v2/src/lgr_quadratic.hpp
+++ v2/src/lgr_quadratic.hpp
@@ -11,10 +11,10 @@ namespace lgr {
 
 Omega_h::Few<Omega_h::LOs, 2> number_p2_nodes(Omega_h::Mesh& mesh);
 
-Omega_h::LOs build_p2_elems2nodes(Omega_h::Mesh& mesh,
+Omega_h::LOs build_p2_ents2nodes(Omega_h::Mesh& mesh, int ent_dim,
     Omega_h::Few<Omega_h::LOs, 2> nodes);
 
-Omega_h::Adj build_p2_nodes2elems(Omega_h::Mesh& mesh,
+Omega_h::Adj build_p2_nodes2ents(Omega_h::Mesh& mesh, int ent_dim,
     Omega_h::Few<Omega_h::LOs, 2> nodes);
 
 Omega_h::Reals build_p2_node_coords(Omega_h::Mesh& mesh,
