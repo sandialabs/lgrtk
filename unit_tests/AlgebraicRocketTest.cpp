6a326b0bc90594433178d81ed10715b4b871ad44
David R. Noble <drnoble@sandia.gov>
2018-11-08 08:53:17 -0700

    Add level set based cylinder geometry model and algebraic rocket model that uses it

--- unit_tests/AlgebraicRocketTest.cpp
+++ unit_tests/AlgebraicRocketTest.cpp
@@ -10,12 +10,25 @@
 
 #include "plato/Plato_AlgebraicRocketModel.hpp"
 #include "plato/Plato_Cylinder.hpp"
+#include "plato/Plato_LevelSetCylinderInBox.hpp"
 
-TEUCHOS_UNIT_TEST(PlatoLGRUnitTests, AlgebraicRocket)
+TEUCHOS_UNIT_TEST(PlatoLGRUnitTests, AlgebraicRocketAnalyticalCylinder)
 {
   const Plato::Scalar ChamberRadius = 0.075; // m
   const Plato::Scalar ChamberLength = 0.65; // m
   auto cylinder = std::make_shared<Plato::Cylinder<Plato::Scalar>>(ChamberRadius, ChamberLength);
+
+  const Plato::AlgebraicRocketInputs<Plato::Scalar> rocketInputs;
+
+  Plato::AlgebraicRocketModel<double> tDriver(rocketInputs, cylinder);
+  tDriver.solve();
+}
+
+TEUCHOS_UNIT_TEST(PlatoLGRUnitTests, AlgebraicRocketLevelSetCylinder)
+{
+  const Plato::Scalar ChamberRadius = 0.075; // m
+  const Plato::Scalar ChamberLength = 0.65; // m
+  auto cylinder = std::make_shared<Plato::LevelSetCylinderInBox<Plato::Scalar>>(ChamberRadius, ChamberLength);
 
   const Plato::AlgebraicRocketInputs<Plato::Scalar> rocketInputs;
 
