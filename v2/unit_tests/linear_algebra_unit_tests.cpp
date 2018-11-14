0821773e5930a3fec865499e87b47014bfa30af9
Dan Ibanez <daibane@sandia.gov>
2018-11-14 10:55:26 -0700

    test BC setting

--- v2/unit_tests/linear_algebra_unit_tests.cpp
+++ v2/unit_tests/linear_algebra_unit_tests.cpp
@@ -3,6 +3,7 @@
 #include <lgr_for.hpp>
 #include <Omega_h_int_scan.hpp>
 #include <Omega_h_array_ops.hpp>
+#include <Omega_h_map.hpp>
 
 TEST(linear_algebra, wikipedia_cg) {
   Omega_h::Write<int> offsets = {0, 2, 4};
@@ -21,7 +22,7 @@ TEST(linear_algebra, wikipedia_cg) {
 }
 
 static void run_fd_cg() {
-  int const ndofs = 8;
+  int const ndofs = 3;
   Omega_h::Write<int> counts(ndofs);
   auto f0 = OMEGA_H_LAMBDA(int const i) {
     if (i == 0 || i == ndofs - 1) counts[i] = 2;
@@ -48,20 +49,21 @@ static void run_fd_cg() {
     OMEGA_H_CHECK(offsets[i + 1] == nz);
   };
   lgr::parallel_for(ndofs, std::move(f2));
-  Omega_h::Write<double> rhs(ndofs, 0.0);
-  double const left_temp = 0.0;
-  double const right_temp = 1.0;
-  rhs.set(0, rhs.get(0) + left_temp);
-  rhs.set(ndofs - 1, rhs.get(ndofs - 1) + right_temp);
-  double const dx = 1.0 / double(ndofs + 1);
-  double const dtemp = right_temp - left_temp;
-  Omega_h::Write<double> known_answer(ndofs, left_temp + dtemp * dx, dx * dtemp);
-  Omega_h::Write<double> computed_answer(ndofs, 0.0);
-  double const tol = 1e-8;
   Omega_h::Graph rows_to_columns(offsets, indices);
   lgr::GlobalMatrix A;
   A.rows_to_columns = rows_to_columns;
   A.entries = values;
+  Omega_h::Write<double> rhs(ndofs, 0.0);
+  double const left_temp = 0.0;
+  double const right_temp = 1.0;
+  double const dx = 1.0 / double(ndofs + 1);
+  double const dtemp = right_temp - left_temp;
+  Omega_h::Write<double> known_answer(ndofs, left_temp, dx * dtemp);
+  Omega_h::LOs bc_rows = {0, ndofs - 1};
+  auto rows_to_bc_rows = Omega_h::invert_injective_map(bc_rows, ndofs);
+  set_boundary_conditions(A, known_answer, rhs, rows_to_bc_rows);
+  Omega_h::Write<double> computed_answer(ndofs, 0.0);
+  double const tol = 1e-8;
   OMEGA_H_CHECK(0 == lgr::conjugate_gradient(A, rhs, known_answer, tol));
   int const niter = conjugate_gradient(A, rhs, computed_answer, tol);
   OMEGA_H_CHECK(niter <= ndofs);
