f8010c9104e11a11758418fc1ca8f638d736e857
Dan Ibanez <daibane@sandia.gov>
2018-11-15 11:00:09 -0700

    starting to redesign internal energy update

--- v2/src/lgr_internal_energy.cpp
+++ v2/src/lgr_internal_energy.cpp
@@ -15,19 +15,63 @@ struct InternalEnergy : public Model<Elem> {
   {
     specific_internal_energy_rate =
       this->point_define("e_dot", "specific internal energy rate", 1,
-          RemapType::PER_UNIT_VOLUME, "0.0");
+          RemapType::NONE, "0.0");
   }
-  std::uint64_t exec_stages() override final { return BEFORE_POSITION_UPDATE | AFTER_CORRECTION; }
+  std::uint64_t exec_stages() override final { return BEFORE_MATERIAL_MODEL | BEFORE_SECONDARIES | AFTER_CORRECTION; }
   char const* name() override final { return "internal energy"; }
-  void before_position_update() override final {
+  void before_material_model() override final {
+    compute_internal_energy_predictor();
+  }
+  void before_secondaries() override final {
+    backtrack_to_midpoint_internal_energy();
+    zero_internal_energy_rate();
+  }
+  void after_correction() override final {
+    contribute_stress_power();
+    correct_internal_energy();
+  }
+  // based on the previous energy and energy rate, compute a predicted
+  // energy using forward Euler. this predicted energy is what material models use
+  void compute_internal_energy_predictor() {
+    OMEGA_H_TIME_FUNCTION;
+    auto const points_to_e = this->points_getset(this->specific_internal_energy);
+    auto const points_to_e_dot = this->points_get(this->specific_internal_energy_rate);
+    auto const dt = this->sim.dt;
+    auto functor = OMEGA_H_LAMBDA(int const point) {
+      auto const e_dot_n = points_to_e_dot[point];
+      auto const e_n = points_to_e[point];
+      auto const e_np1_tilde = e_n + e_dot_n * dt;
+      points_to_e[point] = e_np1_tilde;
+    };
+    parallel_for(this->points(), std::move(functor));
+  }
+  void backtrack_to_midpoint_internal_energy() {
+    OMEGA_H_TIME_FUNCTION;
+    auto const points_to_e = this->points_getset(this->specific_internal_energy);
+    auto const points_to_e_dot = this->points_get(this->specific_internal_energy_rate);
+    auto const dt = this->sim.dt;
+    auto functor = OMEGA_H_LAMBDA(int const point) {
+      auto const e_dot_n = points_to_e_dot[point];
+      auto const e_np1_tilde = points_to_e[point];
+      auto const e_np12 = e_np1_tilde - 0.5 * dt * e_dot_n;
+      points_to_e[point] = e_np1_tilde;
+    };
+    parallel_for(this->points(), std::move(functor));
+  }
+  // zero the rate before other models contribute to it
+  void zero_internal_energy_rate() {
+    OMEGA_H_TIME_FUNCTION;
+    auto const points_to_e_dot = this->points_set(this->specific_internal_energy_rate);
+    Omega_h::fill(points_to_e_dot, 0.0);
+  }
+  void contribute_stress_power() {
+    OMEGA_H_TIME_FUNCTION;
+    auto const points_to_e_dot = this->points_getset(this->specific_internal_energy_rate);
     auto const points_to_grad = this->points_get(this->sim.gradient);
     auto const points_to_rho = this->points_get(this->sim.density);
     auto const points_to_sigma = this->points_get(this->sim.stress);
-    auto const points_to_e = this->points_getset(this->specific_internal_energy);
-    auto const points_to_e_dot = this->points_set(this->specific_internal_energy_rate);
     auto const elems_to_nodes = this->get_elems_to_nodes();
     auto const nodes_to_v = this->sim.get(this->sim.velocity);
-    auto const dt = this->sim.dt;
     auto functor = OMEGA_H_LAMBDA(int const point) {
       auto const elem = point / Elem::points;
       auto const elem_nodes = getnodes<Elem>(elems_to_nodes, elem);
@@ -38,36 +82,22 @@ struct InternalEnergy : public Model<Elem> {
       auto const e_rho_dot_n = inner_product(dvn_dxn, sigma_n);
       auto const rho_n = points_to_rho[point];
       auto const e_dot_n = e_rho_dot_n / rho_n;
-      auto const e_n = points_to_e[point];
-      auto const e_np12 = e_n + e_dot_n * (0.5 * dt);
-      points_to_e[point] = e_np12;
-      points_to_e_dot[point] = e_dot_n;
+      points_to_e_dot[point] += e_dot_n;
     };
     parallel_for(this->points(), std::move(functor));
   }
-  void after_correction() override final {
-    auto const points_to_grad = this->points_get(this->sim.gradient);
-    auto const points_to_rho = this->points_get(this->sim.density);
-    auto const points_to_sigma = this->points_get(this->sim.stress);
+  // using the previous midpoint energy and the current energy rate,
+  // compute the current energy
+  void correct_internal_energy() override final {
+    OMEGA_H_TIME_FUNCTION;
     auto const points_to_e = this->points_getset(this->specific_internal_energy);
-    auto const points_to_e_dot = this->points_set(this->specific_internal_energy_rate);
-    auto const elems_to_nodes = this->get_elems_to_nodes();
-    auto const nodes_to_v = this->sim.get(this->sim.velocity);
+    auto const points_to_e_dot = this->points_get(this->specific_internal_energy_rate);
     auto const dt = this->sim.dt;
     auto functor = OMEGA_H_LAMBDA(int const point) {
-      auto const elem = point / Elem::points;
-      auto const elem_nodes = getnodes<Elem>(elems_to_nodes, elem);
-      auto const v_np1 = getvecs<Elem>(nodes_to_v, elem_nodes);
-      auto const dN_dxnp1 = getgrads<Elem>(points_to_grad, point);
-      auto const dvnp1_dxnp1 = grad<Elem>(dN_dxnp1, v_np1);
-      auto const sigma_np1 = getsymm<Elem>(points_to_sigma, point);
-      auto const e_rho_dot_np1 = inner_product(dvnp1_dxnp1, sigma_np1);
-      auto const rho_np1 = points_to_rho[point];
-      auto const e_dot_np1 = e_rho_dot_np1 / rho_np1;
+      auto const e_dot_np1 = points_to_e_dot[point];
       auto const e_np12 = points_to_e[point];
       auto const e_np1 = e_np12 + e_dot_np1 * (0.5 * dt);
       points_to_e[point] = e_np1;
-      points_to_e_dot[point] = e_dot_np1;
     };
     parallel_for(this->points(), std::move(functor));
   }
