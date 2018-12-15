485d1fe9a68308a9e6974aade39943ef8d4878ea
Dan Ibanez <daibane@sandia.gov>
2018-12-15 10:09:55 -0700

    incorrect attempt at axisymmetry

---
+++ v2/src/lgr_circular_symmetry.hpp
@@ -0,0 +1,25 @@
+#ifndef LGR_CIRCULAR_SYMMETRY_HPP
+#define LGR_CIRCULAR_SYMMETRY_HPP
+
+#include <lgr_element_types.hpp>
+#include <lgr_model.hpp>
+#include <string>
+
+namespace lgr {
+
+template <class Elem>
+ModelBase* circular_symmetry_factory(
+    Simulation& sim, std::string const& name,
+    Omega_h::InputMap& pl);
+
+#define LGR_EXPL_INST(Elem) \
+extern template ModelBase* \
+circular_symmetry_factory<Elem>( \
+    Simulation&, std::string const&, Omega_h::InputMap&);
+LGR_EXPL_INST_ELEMS
+#undef LGR_EXPL_INST
+
+}
+
+#endif
+
