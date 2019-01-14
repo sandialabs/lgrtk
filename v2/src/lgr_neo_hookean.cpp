d6536bbca43f2f295cf4ee488ab50ca56089b183
Dan Ibanez <daibane@sandia.gov>
2019-01-14 12:34:20 -0700

    Rename F remap to POLAR_REMAP

--- v2/src/lgr_neo_hookean.cpp
+++ v2/src/lgr_neo_hookean.cpp
@@ -43,7 +43,7 @@ struct NeoHookean : public Model<Elem> {
         "mu", "shear modulus", 1, RemapType::PER_UNIT_VOLUME, pl, "");
     constexpr auto dim = Elem::dim;
     this->deformation_gradient = this->point_define("F", "deformation gradient",
-        square(dim), RemapType::POSITIVE_DETERMINANT, pl, "I");
+        square(dim), RemapType::POLAR_REMAP, pl, "I");
   }
   std::uint64_t exec_stages() override final { return AT_MATERIAL_MODEL; }
   char const* name() override final { return "neo-Hookean"; }
