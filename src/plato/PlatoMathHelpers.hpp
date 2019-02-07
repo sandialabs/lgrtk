d54b2303a55fe8fdb565ccc900ad7120efb29d45
Miguel A. Aguilo <maguilo@sandia.gov>
2019-02-06 19:29:16 -0700

    implemented fourth-order finite difference diagnostic tool and check partial derivative with respect to controls

--- src/plato/PlatoMathHelpers.hpp
+++ src/plato/PlatoMathHelpers.hpp
@@ -9,27 +9,77 @@
 
 #include <cassert>
 
+#include <Kokkos_Macros.hpp>
+
 #include "plato/PlatoStaticsTypes.hpp"
 
 namespace Plato
 {
 
-/******************************************************************************/
-template<typename ScalarT, typename VectorT>
-void fill(const ScalarT & aInput, const VectorT & aVector)
-/******************************************************************************/
+/******************************************************************************//**
+ * @brief Fill host 1D container with random numbers
+ * @param [in] aLowerBound lower bounds on random numbers
+ * @param [in] aUpperBound upper bounds on random numbers
+ * @param [in] aOutput output 1D container
+**********************************************************************************/
+template<typename VecType>
+inline void random(const Plato::Scalar & aLowerBound, const Plato::Scalar & aUpperBound, VecType & aOutput)
+{
+    unsigned int tRANDOM_SEED = 1;
+    std::srand(tRANDOM_SEED);
+    const Plato::OrdinalType tSize = aOutput.size();
+    for(Plato::OrdinalType tIndex = 0; tIndex < tSize; tIndex++)
+    {
+        const Plato::Scalar tRandNum = static_cast<Plato::Scalar>(std::rand()) / static_cast<Plato::Scalar>(RAND_MAX);
+        aOutput(tIndex) = aLowerBound + ( (aUpperBound - aLowerBound) * tRandNum);
+    }
+}
+// function random
+
+/******************************************************************************//**
+ * @brief Compute inner product
+ * @param [in] aVec1 1D container
+ * @param [in] aVec2 1D container
+ * @return inner product
+**********************************************************************************/
+template<typename VecOneT, typename VecTwoT>
+inline Plato::Scalar dot(VecOneT & aVec1, VecTwoT & aVec2)
+{
+    assert(aVec2.size() == aVec1.size());
+    Plato::Scalar tOutput = 0.;
+    const Plato::OrdinalType tSize = aVec1.size();
+    Kokkos::parallel_reduce(Kokkos::RangePolicy<>(0, tSize),
+                            LAMBDA_EXPRESSION(const Plato::OrdinalType & aIndex, Plato::Scalar & aSum)
+    {
+        aSum += aVec1(aIndex) * aVec2(aIndex);
+    }, tOutput);
+    return (tOutput);
+}
+// function dot
+
+/******************************************************************************//**
+ * @brief Set all the elements to a scalar value
+ * @param [in] aInput scalar value
+ * @param [out] aVector 1D container
+**********************************************************************************/
+template<typename VectorT>
+inline void fill(const Plato::Scalar & aInput, const VectorT & aVector)
 {
     Plato::OrdinalType tNumLocalVals = aVector.size();
     Kokkos::parallel_for(Kokkos::RangePolicy<>(0, tNumLocalVals), LAMBDA_EXPRESSION(const Plato::OrdinalType & aOrdinal)
     {
         aVector(aOrdinal) = aInput;
     }, "fill vector");
-} // function fill
+}
+// function fill
 
-/******************************************************************************/
-template<typename ScalarT, typename VectorT>
-void copy(const Plato::ScalarVectorT<ScalarT> & aInput, const VectorT & aOutput)
-/******************************************************************************/
+/******************************************************************************//**
+ * @brief Copy input 1D container into output 1D container
+ * @param [in] aInput 1D container
+ * @param [out] aOutput 1D container
+**********************************************************************************/
+template<typename VecOneT, typename VecTwoT>
+inline void copy(const VecOneT & aInput, const VecTwoT & aOutput)
 {
     assert(aInput.size() == aOutput.size());
     Plato::OrdinalType tNumLocalVals = aInput.size();
@@ -37,37 +87,52 @@ void copy(const Plato::ScalarVectorT<ScalarT> & aInput, const VectorT & aOutput)
     {
         aOutput(aOrdinal) = aInput(aOrdinal);
     }, "copy vector");
-} // function copy
+}
+// function copy
 
-/******************************************************************************/
-template<typename ScalarT, typename VectorT>
-void scale(const ScalarT & aInput, const VectorT & aVector)
-/******************************************************************************/
+/******************************************************************************//**
+ * @brief Scale all the elements by input scalar value
+ * @param [in] aInput scalar value
+ * @param [out] aOutput 1D container
+**********************************************************************************/
+template<typename VecT>
+inline void scale(const Plato::Scalar & aInput, const VecT & aVector)
 {
     int tNumLocalVals = aVector.size();
     Kokkos::parallel_for(Kokkos::RangePolicy<>(0, tNumLocalVals), LAMBDA_EXPRESSION(const Plato::OrdinalType & aOrdinal)
     {
         aVector(aOrdinal) *= aInput;
     }, "scale vector");
-} // function scale
+}
+// function scale
 
-/******************************************************************************/
-template<typename ScalarT, typename VectorT>
-void axpy(const ScalarT & aAlpha, const VectorT & aInput, const VectorT & aOutput)
-/******************************************************************************/
+/******************************************************************************//**
+ * @brief Update elements of B with scaled values of A, /f$ B = B + alpha*A /f$
+ * @param [in] aAlpha multiplier of 1D container A
+ * @param [in] aInput input 1D container
+ * @param [out] aOutput output 1D container
+**********************************************************************************/
+template<typename VecT>
+inline void axpy(const Plato::Scalar & aAlpha, const VecT & aInput, const VecT & aOutput)
 {
     assert(aInput.size() == aOutput.size());
     Plato::OrdinalType tNumLocalVals = aInput.size();
     Kokkos::parallel_for(Kokkos::RangePolicy<>(0, tNumLocalVals), LAMBDA_EXPRESSION(const Plato::OrdinalType & aOrdinal)
     {
         aOutput(aOrdinal) += aAlpha * aInput(aOrdinal);
-    }, "update vector");
-} // function update
+    }, "Plato::axpy");
+}
+// function axpy
 
-/******************************************************************************/
-template<typename ScalarT, typename VectorT>
-void update(const ScalarT & aAlpha, const VectorT & aInput, const ScalarT & aBeta, const VectorT & aOutput)
-/******************************************************************************/
+/******************************************************************************//**
+ * @brief Update elements of B with scaled values of A, /f$ B = beta*B + alpha*A /f$
+ * @param [in] aAlpha multiplier of 1D container A
+ * @param [in] aInput input 1D container
+ * @param [in] aBeta multiplier of 1D container B
+ * @param [out] aOutput output 1D container
+**********************************************************************************/
+template<typename VecT>
+void update(const Plato::Scalar & aAlpha, const VecT & aInput, const Plato::Scalar & aBeta, const VecT & aOutput)
 {
     assert(aInput.size() == aOutput.size());
     Plato::OrdinalType tNumLocalVals = aInput.size();
@@ -75,14 +140,19 @@ void update(const ScalarT & aAlpha, const VectorT & aInput, const ScalarT & aBet
     {
         aOutput(aOrdinal) = aAlpha * aInput(aOrdinal) + aBeta * aOutput(aOrdinal);
     }, "update vector");
-} // function update
+}
+// function update
 
-/******************************************************************************/
+/******************************************************************************//**
+ * @brief Matrix times vector plus vector
+ * @param [in] aMatrix multiplier of 1D container A
+ * @param [in] aInput input 1D container
+ * @param [out] aOutput output 1D container
+**********************************************************************************/
 template<typename ScalarT>
 void MatrixTimesVectorPlusVector(const Teuchos::RCP<Plato::CrsMatrixType> & aMatrix,
-                                 const Plato::ScalarVectorT<ScalarT> & aVector_a,
-                                 const Plato::ScalarVectorT<ScalarT> & aVector_b)
-/******************************************************************************/
+                                 const Plato::ScalarVectorT<ScalarT> & aInput,
+                                 const Plato::ScalarVectorT<ScalarT> & aOutput)
 {
     if(aMatrix->isBlockMatrix())
     {
@@ -113,10 +183,10 @@ void MatrixTimesVectorPlusVector(const Teuchos::RCP<Plato::CrsMatrixType> & aMat
                     ScalarT tSum = 0.0;
                     for ( auto tDofColIndex = tFromDofColIndex; tDofColIndex < tToDofColIndex; tDofColIndex++ )
                     {
-                        tSum += tEntries(tMatrixEntryIndex) * aVector_a(tDofColIndex);
+                        tSum += tEntries(tMatrixEntryIndex) * aInput(tDofColIndex);
                         tMatrixEntryIndex += 1;
                     }
-                    aVector_b(tDofRowIndex) += tSum;
+                    aOutput(tDofRowIndex) += tSum;
                 }
             }
         }, "BlockMatrix * Vector_a + Vector_b");
@@ -136,12 +206,13 @@ void MatrixTimesVectorPlusVector(const Teuchos::RCP<Plato::CrsMatrixType> & aMat
             for (auto tEntryIndex = tRowStart; tEntryIndex < tRowEnd; tEntryIndex++)
             {
                 auto tColumnIndex = tColIndices(tEntryIndex);
-                tSum += tEntries(tEntryIndex) * aVector_a(tColumnIndex);
+                tSum += tEntries(tEntryIndex) * aInput(tColumnIndex);
             }
-            aVector_b(aRowOrdinal) += tSum;
+            aOutput(aRowOrdinal) += tSum;
         },"Matrix * Vector_a + Vector_b");
     }
-} // function MatrixTimesVectorPlusVector
+}
+// function MatrixTimesVectorPlusVector
 
 } // namespace Plato
 
