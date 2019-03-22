423e58b795c56dc30f63f39562229533219c4531
Dan Ibanez <daibane@sandia.gov>
2019-03-22 16:46:34 -0600

    parameter pack expansion order not defined
     Conflicts:
    	v3/lgr_physics.cpp

--- v3/lgr_print.hpp
+++ v3/lgr_print.hpp
@@ -7,36 +7,8 @@
 
 namespace lgr {
 
-void print_one(std::ostream&, std::string const&);
-void print_one(std::ostream&, char const*);
-void print_one(std::ostream&, bool);
-void print_one(std::ostream&, char);
-void print_one(std::ostream&, signed char);
-void print_one(std::ostream&, unsigned char);
-void print_one(std::ostream&, signed int);
-void print_one(std::ostream&, unsigned int);
-void print_one(std::ostream&, signed long);
-void print_one(std::ostream&, unsigned long);
-void print_one(std::ostream&, signed long long);
-void print_one(std::ostream&, unsigned long long);
-void print_one(std::ostream&, float);
-void print_one(std::ostream&, double);
-void print_one(std::ostream&, vector3<double> v);
-void print_one(std::ostream&, matrix3x3<double> v);
-void print_one(std::ostream&, symmetric3x3<double> v);
-
-template <class ...types>
-void print_parameter_pack_expansion_receiver(types...) {}
-
-template <class T>
-int print_parameter_pack_expansion_print(std::ostream& stream, T argument) {
-  print_one(stream, argument);
-  return 0;
-}
-
-template <class ...types>
-void print(std::ostream& stream, types... arguments) {
-  print_parameter_pack_expansion_receiver(print_parameter_pack_expansion_print(stream, arguments)...);
-}
+std::ostream& operator<<(std::ostream&, vector3<double> v);
+std::ostream& operator<<(std::ostream&, matrix3x3<double> v);
+std::ostream& operator<<(std::ostream&, symmetric3x3<double> v);
 
 }
--- v3/lgr_vtk.cpp
+++ v3/lgr_vtk.cpp
@@ -1,5 +1,6 @@
 #include <sstream>
 #include <fstream>
+#include <iomanip>
 
 #include <lgr_vtk.hpp>
 #include <lgr_print.hpp>
@@ -11,17 +12,17 @@
 namespace lgr {
 
 static void start_vtk_file(std::ostream& stream) {
-  print(stream, "# vtk DataFile Version 3.0\n");
-  print(stream, "vtk output\n");
-  print(stream, "ASCII\n");
-  print(stream, "DATASET UNSTRUCTURED_GRID\n");
+  stream << "# vtk DataFile Version 3.0\n";
+  stream << "vtk output\n";
+  stream << "ASCII\n";
+  stream << "DATASET UNSTRUCTURED_GRID\n";
 }
 
 static void write_vtk_points(std::ostream& stream,
     host_vector<vector3<double>> const& x) {
-  print(stream, "POINTS ", x.size(), " double\n");
+  stream << "POINTS " << x.size() << " double\n";
   for (vector3<double> p : x) {
-    print(stream, p, "\n");
+    stream << p << "\n";
   }
 }
 
@@ -30,50 +31,50 @@ static void write_vtk_cells(std::ostream& stream,
     int_range const& elements,
     int_range const& nodes_in_element,
     host_vector<int> const& element_nodes_to_nodes_vector) {
-  print(stream, "CELLS ", elements.size(), " ", elements.size() * (nodes_in_element.size() + 1), "\n");
+  stream << "CELLS " << elements.size() << " " << (elements.size() * (nodes_in_element.size() + 1)) << "\n";
   auto const elements_to_element_nodes = elements * nodes_in_element;
   auto const element_nodes_to_nodes = element_nodes_to_nodes_vector.cbegin();
   for (auto const element_nodes : elements_to_element_nodes) {
-    print(stream, element_nodes.size());
+    stream << element_nodes.size();
     for (auto const element_node : element_nodes) {
       int const node = element_nodes_to_nodes[element_node];
-      print(stream, " ", node);
+      stream << " " << node;
     }
-    print(stream, "\n");
+    stream << "\n";
   }
-  print(stream, "CELL_TYPES ", elements.size(), "\n");
-  signed char cell_type = -1;
+  stream << "CELL_TYPES " << elements.size() << "\n";
+  int cell_type = -1;
   switch (in.element) {
     case BAR: cell_type = 3; break;
     case TRIANGLE: cell_type = 5; break;
     case TETRAHEDRON: cell_type = 10; break;
   }
   for (int i = 0; i < elements.size(); ++i) {
-    print(stream, cell_type, "\n");
+    stream << cell_type << "\n";
   }
 }
 
 static void write_vtk_point_data(std::ostream& stream, int_range const& nodes) {
-  print(stream, "POINT_DATA ", nodes.size(), "\n");
+  stream << "POINT_DATA " << nodes.size() << "\n";
 }
 
 static void write_vtk_scalars(std::ostream& stream, char const* name, host_vector<double> const& vec) {
-  print(stream, "SCALARS ", name, " double 1\n");
-  print(stream, "LOOKUP_TABLE default\n");
+  stream << "SCALARS " << name << " double 1\n";
+  stream << "LOOKUP_TABLE default\n";
   for (double const val : vec) {
-    print(stream, val, "\n");
+    stream << val << "\n";
   }
 }
 
 static void write_vtk_vectors(std::ostream& stream, char const* name, host_vector<vector3<double>> const& vec) {
-  print(stream, "VECTORS ", name, " double\n");
+  stream << "VECTORS " << name << " double\n";
   for (vector3<double> const val : vec) {
-    print(stream, val, "\n");
+    stream << val << "\n";
   }
 }
 
 static void write_vtk_cell_data(std::ostream& stream, int_range const& elements) {
-  print(stream, "CELL_DATA ", elements.size(), "\n");
+  stream << "CELL_DATA " << elements.size() << "\n";
 }
 
 void file_writer::operator()(
@@ -82,9 +83,10 @@ void file_writer::operator()(
     state const& s
     ) {
   std::stringstream filename_stream;
-  print(filename_stream, prefix, "_", file_output_index, ".vtk");
+  filename_stream << prefix << "_" << file_output_index << ".vtk";
   auto const filename = filename_stream.str();
   std::ofstream stream(filename.c_str());
+  stream << std::scientific << std::setprecision(17);
   start_vtk_file(stream);
   write_vtk_points(stream, s.x);
   write_vtk_cells(stream, in, s.elements, s.nodes_in_element, s.elements_to_nodes);
