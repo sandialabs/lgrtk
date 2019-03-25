7a79d23b69b0d5bb1e82ced9e35ba72d31e5cff1
Dan Ibanez <daibane@sandia.gov>
2019-03-25 10:35:27 -0600

    prepare for safer indices

--- lgr_counting_range.hpp
+++ lgr_counting_range.hpp
@@ -7,13 +7,14 @@ namespace lgr {
 
 template <class T>
 class counting_range : public iterator_range<counting_iterator<T>> {
-  public:
+ public:
+  using difference_type = decltype(T(0) - T(0));
   explicit inline counting_range(T const& first, T const& last):
   iterator_range<counting_iterator<T>>(counting_iterator<T>(first), counting_iterator<T>(last))
   {
   }
-  explicit inline counting_range(T const& last):
-  iterator_range<counting_iterator<T>>(counting_iterator<T>(T(0)), counting_iterator<T>(last))
+  explicit inline counting_range(difference_type const size):
+  iterator_range<counting_iterator<T>>(counting_iterator<T>(T(0)), counting_iterator<T>(T(0) + size))
   {
   }
 };
