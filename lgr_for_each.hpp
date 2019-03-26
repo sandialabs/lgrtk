1035755d6c805f70d2f2d378e08ffdb329c2221e
Dan Ibanez <daibane@sandia.gov>
2019-03-25 19:15:56 -0600

    for_each uses universal reference

--- lgr_for_each.hpp
+++ lgr_for_each.hpp
@@ -3,14 +3,7 @@
 namespace lgr {
 
 template <class Range, class UnaryFunction>
-void for_each(Range const& r, UnaryFunction f) {
-  for (auto it = r.begin(), end = r.end(); it != end; ++it) {
-    f(*it);
-  }
-}
-
-template <class Range, class UnaryFunction>
-void for_each(Range& r, UnaryFunction f) {
+void for_each(Range&& r, UnaryFunction f) {
   for (auto it = r.begin(), end = r.end(); it != end; ++it) {
     f(*it);
   }
