95a41c32fe0c85af91664423691391c1166e958d
Dan Ibanez <daibane@sandia.gov>
2019-03-25 19:06:35 -0600

    move entity collection into domain.cpp

--- lgr_domain.cpp
+++ lgr_domain.cpp
@@ -1,4 +1,8 @@
 #include <lgr_domain.hpp>
+#include <lgr_fill.hpp>
+
+// TEMPORARY
+#include <numeric>
 
 namespace lgr {
 
@@ -12,6 +16,30 @@ void union_domain::mark(host_vector<vector3<double>> const& points, int const ma
   for (auto const& uptr : m_domains) {
     uptr->mark(points, marker, markers);
   }
+}
+
+void collect_domain_entities(
+    int_range const nodes,
+    domain const& domain,
+    host_vector<vector3<double>> const& x_vector,
+    host_vector<int>* entities)
+{
+  host_vector<int> is_on(nodes.size());
+  lgr::fill(is_on, int(0));
+  domain.mark(x_vector, int(1), &is_on);
+  host_vector<int> offsets(nodes.size());
+  std::partial_sum(is_on.cbegin(), is_on.cend(), offsets.begin());
+  int const domain_size = std::accumulate(is_on.cbegin(), is_on.cend(), 0);
+  entities->resize(domain_size);
+  auto const domain_ents_to_ents = entities->begin();
+  auto const nodes_to_offsets = offsets.cbegin();
+  auto const nodes_are_on = is_on.cbegin();
+  auto functor2 = [=] (int const node) {
+    if (nodes_are_on[node]) {
+      domain_ents_to_ents[nodes_to_offsets[node] - 1] = node;
+    }
+  };
+  lgr::for_each(nodes, functor2);
 }
 
 }
