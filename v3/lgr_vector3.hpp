a83927e35b10175193588e001a326edb1ac145fb
Dan Ibanez <daibane@sandia.gov>
2019-03-27 10:15:46 -0600

    move struct_in_vector specials to physics_types

--- v3/lgr_vector3.hpp
+++ v3/lgr_vector3.hpp
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <lgr_struct_in_vector.hpp>
 #include <cmath>
 
 namespace lgr {
@@ -113,44 +112,5 @@ inline Scalar
 normalize(vector3<Scalar> const v) noexcept {
   return v / norm(v);
 }
-
-template <class T, layout L, class Index>
-class struct_in_vector<vector3<T>, L, Index> {
-  array_in_vector<T, 3, L, Index> m_array;
-public:
-  using fundamental_type = T;
-  static constexpr int fundamental_array_size = 3;
-  inline struct_in_vector(decltype(m_array) const& array_in) noexcept
-    :m_array(array_in)
-  {}
-  inline operator vector3<T>() const noexcept {
-    double const a = m_array[0];
-    double const b = m_array[1];
-    double const c = m_array[2];
-    return vector3<T>(a, b, c);
-  }
-  inline void operator=(vector3<double> const value) const noexcept {
-    m_array[0] = value(0);
-    m_array[1] = value(1);
-    m_array[2] = value(2);
-  }
-};
-
-template <class T, layout L, class Index>
-class struct_in_vector<vector3<T> const, L, Index> {
-  array_in_vector<T const, 3, L, Index> m_array;
-public:
-  using fundamental_type = T const;
-  static constexpr int fundamental_array_size = 3;
-  inline struct_in_vector(decltype(m_array) const& array_in) noexcept
-    :m_array(array_in)
-  {}
-  inline operator vector3<T>() const noexcept {
-    double const a = m_array[0];
-    double const b = m_array[1];
-    double const c = m_array[2];
-    return vector3<T>(a, b, c);
-  }
-};
 
 }
