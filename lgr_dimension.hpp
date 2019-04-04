b1e66e0c601bc28e8e774d429aaebb284e7dfbda
Dan Ibanez <daibane@sandia.gov>
2019-04-04 12:58:33 -0600

    draft a little of the dimensioned type

--- lgr_dimension.hpp
+++ lgr_dimension.hpp
@@ -23,7 +23,7 @@ template
   ,int amount_power_in
   ,int intensity_power_in
   >
-class dim_t {
+class dimension {
   public:
   static constexpr int length_power = length_power_in;
   static constexpr int mass_power = mass_power_in;
@@ -55,7 +55,7 @@ dimension_t get_runtime_dimension() {
 template <class left, class right>
 class multiply_dimensions_helper_t {
   public:
-    using type = dim_t<
+    using type = dimension<
       left::length_power + right::length_power,
       left::mass_power + right::mass_power,
       left::time_power + right::time_power,
@@ -71,7 +71,7 @@ using multiply_dimensions_t = typename multiply_dimensions_helper_t<left, right>
 template <class left, class right>
 class divide_dimensions_helper_t {
   public:
-    using type = dim_t<
+    using type = dimension<
       left::length_power - right::length_power,
       left::mass_power - right::mass_power,
       left::time_power - right::time_power,
@@ -86,8 +86,15 @@ using divide_dimensions_t = typename divide_dimensions_helper_t<left, right>::ty
 
 template <class dim, int factor>
 class root_dimension_helper_t {
+  static_assert(dim::length_power % factor == 0, "bad");
+  static_assert(dim::mass_power % factor == 0, "bad");
+  static_assert(dim::time_power % factor == 0, "bad");
+  static_assert(dim::current_power % factor == 0, "bad");
+  static_assert(dim::temperature_power % factor == 0, "bad");
+  static_assert(dim::amount_power % factor == 0, "bad");
+  static_assert(dim::intensity_power % factor == 0, "bad");
   public:
-    using type = dim_t<
+    using type = dimension<
       dim::length_power / factor,
       dim::mass_power / factor,
       dim::time_power / factor,
@@ -103,7 +110,7 @@ using root_dimension_t = typename root_dimension_helper_t<dim, factor>::type;
 template <class dim, int factor>
 class raise_dimension_helper_t {
   public:
-    using type = dim_t<
+    using type = dimension<
       dim::length_power * factor,
       dim::mass_power * factor,
       dim::time_power * factor,
@@ -115,5 +122,158 @@ class raise_dimension_helper_t {
 
 template <class dim, int factor>
 using raise_dimension_t = typename raise_dimension_helper_t<dim, factor>::type;
+
+template <class T, class Dimension>
+class dimensioned {
+  T m_impl;
+  public:
+  constexpr inline dimensioned(T const in):m_impl(in) noexcept {}
+  constexpr inline dimensioned() noexcept = default;
+  constexpr inline operator T() noexcept { return m_impl; }
+};
+
+template
+  <class LeftT
+  ,class RightT
+  ,class Dimensions
+  >
+constexpr inline
+operator+
+   (dimensioned<LeftT, Dimensions> const left
+   ,dimensioned<RightT, Dimensions> const right
+   ) -> auto
+{
+  return dimensioned
+    <decltype(LeftT() * RightT())
+    ,Dimensions
+    >
+  ((LeftT(left) + RightT(right)));
+}
+
+template
+  <class LeftT
+  ,class RightT
+  ,class Dimensions
+  >
+constexpr inline
+operator-
+   (dimensioned<LeftT, Dimensions> const left
+   ,dimensioned<RightT, Dimensions> const right
+   ) -> auto
+{
+  return dimensioned
+    <decltype(LeftT() * RightT())
+    ,Dimensions
+    >
+  ((LeftT(left) + RightT(right)));
+}
+
+template
+  <class LeftT
+  ,class RightT
+  ,class LeftDimensions
+  ,class RightDimensions
+  >
+constexpr inline
+operator*
+   (dimensioned<LeftT, LeftDimensions> const left
+   ,dimensioned<RightT, RightDimensions> const right
+   ) -> auto
+{
+  return dimensioned
+    <decltype(LeftT() * RightT())
+    ,multiply_dimensions_t<LeftDimensions, RightDimensions>
+    >
+  ((LeftT(left) * RightT(right)));
+}
+
+template
+  <class LeftT
+  ,class RightT
+  ,class LeftDimensions
+  ,class RightDimensions
+  >
+constexpr inline
+operator/
+   (dimensioned<LeftT, LeftDimensions> const left
+   ,dimensioned<RightT, RightDimensions> const right
+   ) -> auto
+{
+  return dimensioned
+    <decltype(LeftT() / RightT())
+    ,divide_dimensions_t<LeftDimensions, RightDimensions>
+    >
+  ((LeftT(left) / RightT(right)));
+}
+
+template
+  <class T
+  ,class Dimensions
+  >
+constexpr inline
+sqrt
+   (dimensioned<T, Dimensions> const x
+   ) -> auto
+{
+  using std::sqrt;
+  return dimensioned
+    <decltype(sqrt(T()))
+    ,root_dimension_t<Dimensions, 2>
+    >
+  (sqrt(T(x)));
+}
+
+template
+  <class T
+  ,class Dimensions
+  >
+constexpr inline
+cbrt
+   (dimensioned<T, Dimensions> const x
+   ) -> auto
+{
+  using std::sqrt;
+  return dimensioned
+    <decltype(cbrt(T()))
+    ,root_dimension_t<Dimensions, 3>
+    >
+  (cbrt(T(x)));
+}
+
+template
+  <class LeftT
+  ,class RightT
+  ,class LeftDimensions
+  ,class RightDimensions
+  >
+constexpr inline
+inner_product
+   (dimensioned<LeftT, LeftDimensions> const left
+   ,dimensioned<RightT, RightDimensions> const right
+   ) -> auto
+{
+  return dimensioned
+    <decltype(inner_product(LeftT(), RightT()))
+    ,multiply_dimensions_t<LeftDimensions, RightDimensions>
+    >
+  (inner_product(LeftT(left), RightT(right)));
+}
+
+template
+  <class T
+  ,class Dimensions
+  >
+constexpr inline
+norm
+   (dimensioned<T, Dimensions> const x
+   ) -> auto
+{
+  using std::sqrt;
+  return dimensioned
+    <decltype(norm(T()))
+    ,Dimensions
+    >
+  (norm(T(x)));
+}
 
 }
