9a8a8d53dee6f97da6b6d6df1734096e6957bffa
Dan Ibanez <daibane@sandia.gov>
2019-04-12 15:52:31 -0600

    find-replace is dangerous

--- v3/lgr_adapt.cpp
+++ v3/lgr_adapt.cpp
@@ -19,10 +19,10 @@ static void LGR_NOINLINE update_bar_badness(state& s) {
 /* Per:
    Shewchuk, Jonathan Richard.
    "What is a good linear finite element?
-    interpolation, conditioning, anisotropy, and badness measures (preprint)."
+    interpolation, conditioning, anisotropy, and quality measures (preprint)."
    University of California at Berkeley 73 (2002): 137.
    
-   A scale-invariant badness measure correlated with matrix conditioning
+   A scale-invariant quality measure correlated with matrix conditioning
    (and the stable explicit time step) for triangles is area divided by
    the square of the (root-mean-squared edge length).
    We also use the fact that we already have area computed and that basis
@@ -70,17 +70,20 @@ static void LGR_NOINLINE update_triangle_badness(state& s) {
 /* Per:
    Shewchuk, Jonathan Richard.
    "What is a good linear finite element?
-    interpolation, conditioning, anisotropy, and badness measures (preprint)."
+    interpolation, conditioning, anisotropy, and quality measures (preprint)."
    University of California at Berkeley 73 (2002): 137.
    
-   A scale-invariant badness measure correlated with matrix conditioning
+   A scale-invariant quality measure correlated with matrix conditioning
    (and the stable explicit time step) for tetrahedra is volume divided by
    the (root-mean-squared side area) to the (3/2) power.
    We also use the fact that we already have volume computed and that basis
    gradient magnitudes relate to opposite side areas.
+
    Furthermore, we raise the entire quantity to the fourth power in order to
    avoid having to compute any roots, since only relative comparison of
    qualities is necessary.
+   
+   As such, our "badness" is the inverse of this quality measure to the fourth power
   */
 static void LGR_NOINLINE update_tetrahedron_badness(state& s) {
   auto const points_to_V = s.V.cbegin();
