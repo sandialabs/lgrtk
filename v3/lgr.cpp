b72e7b0534db402c68eb204918257f5f705a3b97
Dan Ibanez <daibane@sandia.gov>
2019-04-12 13:10:24 -0600

    go back to popular cases

--- v3/lgr.cpp
+++ v3/lgr.cpp
@@ -886,7 +886,7 @@ static void LGR_NOINLINE triple_point() {
 }
 
 int main() {
-  if ((0)) lgr::elastic_wave();
+  if ((1)) lgr::elastic_wave();
   if ((0)) lgr::gas_expansion();
   if ((0)) lgr::spinning_square();
   if ((0)) lgr::Cooks_membrane();
@@ -899,11 +899,11 @@ int main() {
   if ((0)) lgr::twisting_column();
   if ((0)) lgr::tet_piston();
   if ((0)) lgr::Noh_1D();
-  if ((0)) lgr::Noh_2D();
+  if ((1)) lgr::Noh_2D();
   if ((0)) lgr::Noh_3D();
   if ((0)) lgr::composite_Noh_3D();
   if ((0)) lgr::spinning_composite_cube();
-  if ((0)) lgr::twisting_composite_column();
+  if ((1)) lgr::twisting_composite_column();
   if ((0)) lgr::Sod_1D();
-  if ((1)) lgr::triple_point();
+  if ((0)) lgr::triple_point();
 }
