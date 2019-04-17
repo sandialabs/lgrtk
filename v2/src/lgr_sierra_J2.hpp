7b6a2731d916e860ed57d9c00e413c629ed8c369
Alejandro Mota <amota@sandia.gov>
2019-04-17 16:23:34 -0700

    Cosmetic changes.

--- v2/src/lgr_sierra_J2.hpp
+++ v2/src/lgr_sierra_J2.hpp
@@ -8,11 +8,14 @@
 namespace lgr {
 
 template <class Elem>
-ModelBase* sierra_J2_factory(
-    Simulation& sim, std::string const& name, Omega_h::InputMap& pl);
+ModelBase*
+sierra_J2_factory(
+    Simulation&        sim,
+    std::string const& name,
+    Omega_h::InputMap& pl);
 
-#define LGR_EXPL_INST(Elem)                                                    \
-  extern template ModelBase* sierra_J2_factory<Elem>(                          \
+#define LGR_EXPL_INST(Elem)                           \
+  extern template ModelBase* sierra_J2_factory<Elem>( \
       Simulation&, std::string const&, Omega_h::InputMap&);
 LGR_EXPL_INST_ELEMS
 #undef LGR_EXPL_INST
