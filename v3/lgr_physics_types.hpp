2d35ad06590436305b4241a67a0acf4a9adde8a1
Dan Ibanez <daibane@sandia.gov>
2019-04-17 10:20:57 -0600

    move struct_in_vector<vector3>

--- v3/lgr_physics_types.hpp
+++ v3/lgr_physics_types.hpp
@@ -10,45 +10,6 @@
 namespace lgr {
 
 template <class T, layout L, class Index>
-class struct_in_vector<vector3<T>, L, Index> {
-  array_in_vector<T, 3, L, Index> m_array;
-public:
-  using fundamental_type = T;
-  static constexpr int fundamental_array_size = 3;
-  inline struct_in_vector(decltype(m_array) const& array_in) noexcept
-    :m_array(array_in)
-  {}
-  inline operator vector3<T>() const noexcept {
-    T const a = m_array[0];
-    T const b = m_array[1];
-    T const c = m_array[2];
-    return vector3<T>(a, b, c);
-  }
-  inline void operator=(vector3<T> const value) const noexcept {
-    m_array[0] = value(0);
-    m_array[1] = value(1);
-    m_array[2] = value(2);
-  }
-};
-
-template <class T, layout L, class Index>
-class struct_in_vector<vector3<T> const, L, Index> {
-  array_in_vector<T const, 3, L, Index> m_array;
-public:
-  using fundamental_type = T const;
-  static constexpr int fundamental_array_size = 3;
-  inline struct_in_vector(decltype(m_array) const& array_in) noexcept
-    :m_array(array_in)
-  {}
-  inline operator vector3<T>() const noexcept {
-    T const a = m_array[0];
-    T const b = m_array[1];
-    T const c = m_array[2];
-    return vector3<T>(a, b, c);
-  }
-};
-
-template <class T, layout L, class Index>
 class struct_in_vector<matrix3x3<T>, L, Index> {
   array_in_vector<T, 9, L, Index> m_array;
 public:
