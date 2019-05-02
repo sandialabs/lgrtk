bb5bf6868b9c6c0dd2fcaf773ac6713295cafe27
Dan Ibanez <daibane@sandia.gov>
2019-05-02 11:09:24 -0600

    triple-point runs with stabilized!

--- lgr.cpp
+++ lgr.cpp
@@ -837,7 +837,13 @@ static void LGR_NOINLINE triple_point() {
   in.domains[right_top] = std::move(right_top_domain);
   in.enable_viscosity = true;
   in.linear_artificial_viscosity = 0.5;
-  in.enable_adapt = true;
+  in.enable_adapt = false;
+  in.enable_nodal_energy[left] = true;
+  in.enable_nodal_energy[right_bottom] = true;
+  in.enable_nodal_energy[right_top] = true;
+  in.c_tau[left] = 1.0;
+  in.c_tau[right_bottom] = 1.0;
+  in.c_tau[right_top] = 1.0;
   run(in);
 }
 
@@ -860,6 +866,6 @@ int main() {
   if ((0)) lgr::composite_Noh_3D();
   if ((0)) lgr::spinning_composite_cube();
   if ((0)) lgr::twisting_composite_column();
-  if ((1)) lgr::Sod_1D();
-  if ((0)) lgr::triple_point();
+  if ((0)) lgr::Sod_1D();
+  if ((1)) lgr::triple_point();
 }
