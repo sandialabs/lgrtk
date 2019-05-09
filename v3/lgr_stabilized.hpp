4bdd33237df8b74c24e43fa1b4897e0e102ed689
Dan Ibanez <daibane@sandia.gov>
2019-05-09 14:50:46 -0600

    better encapsulate stabilized code

--- v3/lgr_stabilized.hpp
+++ v3/lgr_stabilized.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <lgr_mesh_indices.hpp>
+#include <lgr_device_vector.hpp>
 
 namespace lgr {
 
@@ -8,15 +9,17 @@ class input;
 class state;
 
 void update_sigma_with_p_h(state& s, material_index const material);
-void update_v_prime(input const& in, state& s, material_index const material);
-void update_q(input const& in, state& s, material_index const material);
-void update_p_h_W(state& s, material_index const material);
-void update_e_h_W(state& s, material_index const material);
-void update_p_h_dot(state& s, material_index const);
-void update_e_h_dot(state& s, material_index const);
+void update_p_h(state& s, double const dt,
+    material_index const material,
+    device_vector<double, node_index> const& old_p_h_vector);
+void update_e_h(state& s, double const dt,
+    material_index const material,
+    device_vector<double, node_index> const& old_e_h_vector);
 void nodal_ideal_gas(input const& in, state& s, material_index const);
 void update_nodal_density(state& s, material_index const);
 void interpolate_K(state& s, material_index const);
 void interpolate_rho(state& s, material_index const);
+void update_p_h_dot_from_a(input const& in, state& s, material_index const material);
+void update_e_h_dot_from_a(input const& in, state& s, material_index const material);
 
 }
