b0a418f10d8eb71e1aaffcfb28522a503b9a5257
Dan Ibanez <daibane@sandia.gov>
2019-05-31 16:07:19 -0600

    progress

--- hpc_transform_reduce.hpp
+++ hpc_transform_reduce.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <hpc_execution.hpp>
+
 namespace hpc {
 
 template <class Range, class T, class BinaryOp, class UnaryOp>
@@ -14,7 +16,8 @@ HPC_ALWAYS_INLINE HPC_HOST_DEVICE T transform_reduce(
 }
 
 template <class Range, class T, class BinaryOp, class UnaryOp>
-T transform_reduce(
+HPC_NOINLINE T
+transform_reduce(
     serial_policy, Range const& range, T init, BinaryOp binary_op, UnaryOp unary_op) {
   auto first = range.begin();
   auto const last = range.end();
@@ -23,5 +26,27 @@ T transform_reduce(
   }
   return init;
 }
+
+#ifdef HPC_CUDA
+
+namespace impl {
+
+template <class Iterator, class T, class BinaryOp, class UnaryOp>
+T
+transform_reduce(
+    cuda_policy policy, Iterator first, Iterator last, T init, BinaryOp binary_op, UnaryOp unary_op) {
+  ::hpc::impl::transform_reduce(policy, range.begin(), range.end(), init, binary_op, unary_op);
+}
+
+}
+
+template <class Range, class T, class BinaryOp, class UnaryOp>
+HPC_NOINLINE T
+transform_reduce(
+    cuda_policy policy, Range const& range, T init, BinaryOp binary_op, UnaryOp unary_op) {
+  ::hpc::impl::transform_reduce(policy, range.begin(), range.end(), init, binary_op, unary_op);
+}
+
+#endif
 
 }
