c0adbbae2cc6eee4494894923ebf89285fd17f3c
Alejandro Mota <amota@sandia.gov>
2020-01-15 18:12:56 -0800

    Add skeleton otm functions
    Also, delete otm directory. Since the code is small,
    it's easier to manage that way.

---
+++ v3/otm_meshless.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace lgr {
+
+class state;
+class input;
+
+void initialize_meshless_V(state& s);
+void initialize_meshless_grad_N(state& s);
+void update_meshless_h_min_inball(input const&, state& s);
+void update_meshless_h_art(state& s);
+
+}
