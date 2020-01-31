c6d6237b4b641fe8830962f8d195feee90a95041
Julia Plews <japlews@sandia.gov>
2020-01-31 12:03:43 -0700

    OTM: invert connectivities for assembly routines.
      Fix search call to fill data structures, update results checking function in
      unit test.
    
      Disable connectivity checking for Exodus mesh while we develop a better
      search algorithm.

--- otm_search.hpp
+++ otm_search.hpp
@@ -7,7 +7,9 @@ void initialize_otm_search();
 
 void finalize_otm_search();
 
-void do_otm_point_node_search(lgr::state &s);
+void do_otm_point_node_search(lgr::state &s, int max_support_nodes_per_point);
+
+void invert_otm_point_node_relations(lgr::state & s);
 
 }
 }
--- otm_tet2meshless.cpp
+++ otm_tet2meshless.cpp
@@ -13,8 +13,6 @@ namespace lgr {
 
 void convert_tet_mesh_to_meshless(state& st)
 {
-  using nodes_in_elem_size_type = hpc::counting_range<node_in_element_index>::size_type;
-
   auto num_points = st.points.size();
   auto num_nodes_in_support = st.nodes_in_element.size();
   st.points_to_supported_nodes.resize(num_points * num_nodes_in_support);
@@ -26,8 +24,8 @@ void convert_tet_mesh_to_meshless(state& st)
   auto points_in_element = st.points_in_element;
   auto elements_to_points = st.elements * st.points_in_element;
 
-  hpc::device_vector<point_index> nodes_in_support_counts(st.points.size(),
-      point_index(st.nodes_in_element.size()));
+  hpc::device_vector<int, point_index> nodes_in_support_counts(st.points.size(),
+      st.nodes_in_element.size());
   st.nodes_in_support.assign_sizes(nodes_in_support_counts);
 
   auto support_nodes_to_nodes = st.points_to_supported_nodes.begin();
@@ -40,28 +38,20 @@ void convert_tet_mesh_to_meshless(state& st)
     auto cur_elem_points = elements_to_points[element];
     auto element_nodes = elements_to_element_nodes[element];
 
-    constexpr nodes_in_elem_size_type max_num_elem_nodes = 10;
-    hpc::array<node_index, max_num_elem_nodes> cur_elem_nodes;
-
-    hpc::position<double> avg_coord(0., 0., 0.);
-    for (auto n : nodes_in_element)
-    {
-      auto cur_elem_node_offset = element_nodes[n];
-      auto node = element_nodes_to_nodes[cur_elem_node_offset];
-      cur_elem_nodes[n] = node;
-      avg_coord += nodes_to_x[node].load();
-    }
-    avg_coord /= nodes_in_element.size();
-
     for (auto&& element_point : points_in_element)
     {
+      hpc::position<double> avg_coord(0., 0., 0.);
       auto point = cur_elem_points[element_point];
-      mat_pts_to_x[point].store(avg_coord);
       auto&& point_support_nodes = nodes_in_support[point];
       for (auto&& n : nodes_in_element)
       {
-        support_nodes_to_nodes[point_support_nodes[n]] = cur_elem_nodes[n];
+        auto cur_elem_node_offset = element_nodes[n];
+        auto node = element_nodes_to_nodes[cur_elem_node_offset];
+        avg_coord += nodes_to_x[node].load();
+        support_nodes_to_nodes[point_support_nodes[n]] = node;
       }
+      avg_coord /= nodes_in_element.size();
+      mat_pts_to_x[point] = avg_coord;
     }
   };
 
