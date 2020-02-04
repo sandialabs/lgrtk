6e58b1a2cad0c9eec2f7df936c860b999ead5fe7
Julia Plews <japlews@sandia.gov>
2020-02-04 09:32:19 -0700

    OTM: make connectivity inversion callable from outside search

---
+++ v3/otm_meshing.cpp
@@ -0,0 +1,59 @@
+#include <hpc_algorithm.hpp>
+#include <hpc_atomic.hpp>
+#include <hpc_execution.hpp>
+#include <hpc_macros.hpp>
+#include <hpc_numeric.hpp>
+#include <hpc_range.hpp>
+#include <hpc_vector.hpp>
+#include <lgr_mesh_indices.hpp>
+#include <lgr_meshing_sort.hpp>
+#include <lgr_state.hpp>
+
+namespace lgr {
+
+void invert_otm_point_node_relations(lgr::state &s)
+{
+  auto points_to_point_nodes = s.nodes_in_support.cbegin();
+  auto point_nodes_to_nodes = s.points_to_supported_nodes.cbegin();
+  hpc::device_vector<int, node_index> node_point_counts(s.nodes.size(), 0);
+  auto counts = node_point_counts.begin();
+  auto point_count_functor = [=] HPC_DEVICE(point_index const point) {
+    auto point_node_range = points_to_point_nodes[point];
+    for(auto point_node : point_node_range) {
+      auto node = point_nodes_to_nodes[point_node];
+      hpc::atomic_ref<int> count(counts[node]);
+      count++;
+    }
+  };
+  hpc::for_each(hpc::device_policy(), s.points, point_count_functor);
+  auto total_node_points = hpc::reduce(hpc::device_policy(), node_point_counts, 0);
+
+  s.nodes_to_influenced_points.resize(node_point_index(total_node_points));
+  s.node_influenced_points_to_supporting_nodes.resize(node_point_index(total_node_points));
+  s.points_in_influence.assign_sizes(node_point_counts);
+  hpc::fill(hpc::device_policy(), node_point_counts, 0);
+  auto node_points_to_points = s.nodes_to_influenced_points.begin();
+  auto points_in_influence = s.points_in_influence.cbegin();
+  auto node_points_to_supporting_nodes = s.node_influenced_points_to_supporting_nodes.begin();
+  auto node_point_fill_functor = [=] HPC_DEVICE(point_index const point)
+  {
+    auto const point_nodes = points_to_point_nodes[point];
+    for (auto ni = 0; ni < point_nodes.size(); ++ni)
+    {
+      const auto point_node = point_nodes[ni];
+      node_index const node = point_nodes_to_nodes[point_node];
+      hpc::atomic_ref<int> count(counts[node]);
+      int const offset = count++;
+      auto const node_points_range = points_in_influence[node];
+      auto const node_point = node_points_range[node_in_support_index(offset)];
+      node_points_to_points[node_point] = point;
+      node_points_to_supporting_nodes[node_point] = ni;
+    }
+  };
+
+  hpc::for_each(hpc::device_policy(), s.points, node_point_fill_functor);
+
+  sort_node_relations(s, s.points_in_influence, s.nodes_to_influenced_points, s.node_influenced_points_to_supporting_nodes);
+}
+
+}
---
+++ v3/otm_meshing.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace lgr {
+class state;
+}
+
+namespace lgr {
+
+void invert_otm_point_node_relations(lgr::state &s);
+
+}
+
