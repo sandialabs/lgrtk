9c261ae9d9c0e0bd72cf4c2baeb73f5f3516dfd4
Brandon Talamini <btalami@sandia.gov>
2020-02-10 15:51:36 -0800

    Variational J2 WIP
    Model currently functions as a Hencky elasticity model.
    The interface for the constitutive law has been extended to
    pass some of the new needed quantities.
    There is no plumbing to input new parameters in an application yet.

--- v3/j2/hardening.hpp
+++ v3/j2/hardening.hpp
@@ -7,9 +7,16 @@ namespace lgr {
 namespace j2 {
 
 struct Properties {
+  double K;
+  double G;
+
   double S0;
   double n;
   double eps0;
+
+  double Svis0;
+  double m;
+  double eps_dot0;
 };
 
 
@@ -21,6 +28,15 @@ FlowStrength(Properties props, double eqps);
 
 double
 HardeningRate(Properties props, double eqps);
+
+double
+ViscoplasticDualKineticPotential(Properties props, double delta_eqps, double dt);
+
+double
+ViscoplasticStress(Properties props, double delta_eqps, double dt);
+
+double
+ViscoplasticHardeningRate(Properties props, double delta_eqps, double dt);
 
 
 } // namespace j2
