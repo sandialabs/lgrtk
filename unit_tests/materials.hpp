f1acfcc4b0b155c495c063a9d17cf463ab442a4e
Brandon Talamini <btalami@sandia.gov>
2020-02-14 11:41:14 -0800

    Get variational J2 plasticity to pass consistency test
    * Fixed several bugs in model
    * Fixed some errors in the test, where state variables were
      being changed between finite difference perturbations.
      The state variables need to be reset to the initial condition
      prior to each constitutive update call, of course.

--- unit_tests/materials.hpp
+++ unit_tests/materials.hpp
@@ -8,7 +8,7 @@ namespace lgr {
 HPC_DEVICE void neo_Hookean_point(hpc::matrix3x3<double> const &F, double const K, double const G,
     hpc::symmetric3x3<double> &sigma, double &Keff, double& Geff, double& potential);
 
-HPC_DEVICE void variational_J2_point(hpc::matrix3x3<double> const &F, j2::Properties props,
+HPC_DEVICE void variational_J2_point(hpc::matrix3x3<double> const &F, j2::Properties props, double dt,
     hpc::symmetric3x3<double> &sigma, double &Keff, double& Geff, double& potential, hpc::matrix3x3<double> &Fp, double& eqps);
 }
 
