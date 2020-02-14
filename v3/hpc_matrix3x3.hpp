40799a407b5184e1ab118dc112ffe4c958a995e9
Brandon Talamini <btalami@sandia.gov>
2020-02-14 11:39:07 -0800

    Add xtor for making a matrix from a symmetric matrix

--- v3/hpc_matrix3x3.hpp
+++ v3/hpc_matrix3x3.hpp
@@ -9,6 +9,11 @@
 
 namespace hpc {
 
+// forward declarations
+template <typename Scalar>
+struct symmetric3x3;
+
+
 template <typename Scalar>
 class matrix3x3 {
 public:
@@ -30,6 +35,10 @@ public:
     raw[2][0] = g;
     raw[2][1] = h;
     raw[2][2] = i;
+  }
+  HPC_ALWAYS_INLINE HPC_HOST_DEVICE constexpr matrix3x3(symmetric3x3<Scalar> const& A) noexcept
+  {
+    matrix3x3(A(0,0), A(0,1), A(0,2), A(1,0), A(1,1), A(1,2), A(2,0), A(2,1), A(2,2));
   }
   HPC_ALWAYS_INLINE matrix3x3() noexcept = default;
   HPC_ALWAYS_INLINE HPC_HOST_DEVICE static constexpr matrix3x3 identity() noexcept {
