569c93ebdafcaf7c177095c31666f0f5a2956a35
David Zwick <dzwick@sandia.gov>
2020-02-17 13:54:50 -0700

    Added a uniform random initialization function for conditions

--- v2/src/lgr_condition.cpp
+++ v2/src/lgr_condition.cpp
@@ -3,6 +3,8 @@
 #include <Omega_h_scalar.hpp>
 #include <lgr_riemann.hpp>
 #include <lgr_simulation.hpp>
+#include <lgr_for.hpp>
+#include <cstdlib>
 
 namespace lgr {
 
@@ -29,6 +31,71 @@ LGR_RIEMANN_EXPR(velocity)
 LGR_RIEMANN_EXPR(density)
 LGR_RIEMANN_EXPR(pressure)
 #undef LGR_RIEMANN_EXPR
+
+#define IM1 2147483563
+#define IM2 2147483399
+#define AM (1.0/IM1)
+#define IMM1 (IM1-1)
+#define IA1 40014
+#define IA2 40692
+#define IQ1 53668
+#define IQ2 52774
+#define IR1 12211
+#define IR2 3791
+#define NTAB 32
+#define NDIV (1+IMM1/NTAB)
+#define EPS 1.2e-7
+#define RNMX (1.0-EPS)
+
+/*
+*/
+static double ran2(long *idum) {
+ //Long period (> 2 × 1018) random number generator of L’Ecuyer with Bays-Durham shuffle
+ //and added safeguards. Returns a uniform random deviate between 0.0 and 1.0 (exclusive of
+ //the endpoint values). Call with idum a negative integer to initialize; thereafter, do not alter
+ //idum between successive deviates in a sequence. RNMX should approximate the largest floating
+ //value that is less than 1.
+   int j;
+   long k;
+   static long idum2=123456789;
+   static long iy=0;
+   static long iv[NTAB];
+   double temp;
+   if (*idum <= 0) { //Initialize.
+      if (-(*idum) < 1) *idum=1; // Be sure to prevent idum = 0.
+      else *idum = -(*idum);
+      idum2=(*idum);
+      for (j=NTAB+7;j>=0;j--) { // Load the shuffle table (after 8 warm-ups).
+         k=(*idum)/IQ1;
+         *idum=IA1*(*idum-k*IQ1)-k*IR1;
+         if (*idum < 0) *idum += IM1;
+         if (j < NTAB) iv[j] = *idum;
+      }
+      iy=iv[0];
+   }
+   k=(*idum)/IQ1; // Start here when not initializing.
+   *idum=IA1*(*idum-k*IQ1)-k*IR1; // Compute idum=(IA1*idum) % IM1 without
+   if (*idum < 0) *idum += IM1; // overflows by Schrage’s method.
+   k=idum2/IQ2;
+   idum2=IA2*(idum2-k*IQ2)-k*IR2; // Compute idum2=(IA2*idum) % IM2 likewise.
+   if (idum2 < 0) idum2 += IM2;
+   j=iy/NDIV; // Will be in the range 0..NTAB-1.
+   iy=iv[j]-idum2; // Here idum is shuffled, idum and idum2 are
+   iv[j] = *idum; // combined to generate output.
+   if (iy < 1) iy += IMM1;
+   if ((temp=AM*iy) > RNMX) return RNMX; // Because users don’t expect endpoint values.
+   else return temp;
+}
+
+static Omega_h::any uniform_random(std::vector<Omega_h::any>& args) {
+    double rnum = ran2(new long(1));
+    const double lvalue = Omega_h::any_cast<double>(args.at(0));
+    const double rvalue = Omega_h::any_cast<double>(args.at(1));
+    const double diff = rvalue - lvalue;
+    rnum *= diff;
+    rnum += lvalue;
+    return rnum;
+}
 
 void Condition::init() {
   auto vars_used = Omega_h::math_lang::get_symbols_used(str);
@@ -83,6 +150,7 @@ void Condition::forget_disc() {
 }
 
 void Condition::learn_disc() {
+  ran2(new long(-1)); // Call one time with negative number to initialize random numbers
   env = decltype(env)(support->count(), support->subset->disc.dim());
   env.register_function("riemann_velocity", riemann_expr_velocity);
   env.register_function("riemann_density", riemann_expr_density);
@@ -96,6 +164,9 @@ void Condition::learn_disc() {
       env.register_variable(name, value);
     }
   }
+  // Copy some functions to both expression environments
+  env.register_function("uniform_random", uniform_random);
+  user_env.register_function("uniform_random", uniform_random);
   if (uses_old_vals && env.variables.count(field->short_name)) {
     Omega_h_fail(
         "expr for field \"%s\" contains \"%s\", which is also a "
