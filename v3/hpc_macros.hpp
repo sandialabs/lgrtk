64141af590bd6ca69659c806a7f1228c5d36e72b
Alejandro Mota <amota@sandia.gov>
2020-02-25 13:07:09 -0800

    Add debug helper macros in HPC

--- v3/hpc_macros.hpp
+++ v3/hpc_macros.hpp
@@ -17,3 +17,22 @@
 #define HPC_NOINLINE
 #endif
 #define HPC_ALWAYS_INLINE __attribute__((always_inline)) inline
+
+// Macros for debugging
+#ifdef __CUDACC__
+#define HPC_TRACE_IMPL(msg, ...)                                      \
+  do {                                                                \
+  } while (0)
+#else
+#include <iostream>
+#define HPC_TRACE_IMPL(msg, ...)                                      \
+  do {                                                                \
+    std::ostringstream omsg;                                          \
+    omsg << "********** HPC_TRACE at ";                               \
+    omsg << __FILE__ << " +" << __LINE__ << '\n' << msg << '\n';      \
+    std::cerr << "********** HPC_TRACE at ";                          \
+    std::cerr << __FILE__ << " +" << __LINE__ << "\n" << msg << '\n'; \
+  } while (0)
+#endif
+
+#define HPC_TRACE(...) HPC_TRACE_IMPL(__VA_ARGS__, "")
