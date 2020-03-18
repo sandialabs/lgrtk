60c909213327042e3a034f94a303f13987ccc78c
Alejandro Mota <amota@sandia.gov>
2020-03-17 17:09:42 -0700

    Rework point interpolator
    Fix reinitialization of point coordinates and move Gauss-Legendre
    locations to the interpolator itself.

--- otm_exodus.hpp
+++ otm_exodus.hpp
@@ -22,56 +22,6 @@
 namespace lgr
 {
 
-HPC_ALWAYS_INLINE hpc::position<double>
-tetrahedron_local_to_global_coord(const hpc::array<hpc::position<double>, 4>& node_coords,
-    const hpc::position<double> &parametric_coord)
-{
-  return parametric_coord(0) * node_coords[0] + parametric_coord(1) * node_coords[1]
-      + parametric_coord(2) * node_coords[2]
-      + (1.0 - parametric_coord(0) - parametric_coord(1) - parametric_coord(2)) * node_coords[3];
-}
-
-struct tet_gauss_points_to_material_points
-{
-  tet_gauss_points_to_material_points(
-      const hpc::pinned_vector<hpc::position<double>, point_node_index> &gauss_pts) :
-      gauss_points(gauss_pts.size())
-  {
-    hpc::copy(gauss_pts, gauss_points);
-  }
-
-  void operator()(hpc::counting_range<point_index> const points,
-      hpc::device_range_sum<point_node_index, point_index> const &points_to_point_nodes,
-      hpc::device_vector<node_index, point_node_index> const &point_nodes_to_nodes,
-      hpc::device_array_vector<hpc::position<double>, node_index> const &x,
-      hpc::device_array_vector<hpc::position<double>, point_index> &xp)
-  {
-    auto pt_to_pt_nodes = points_to_point_nodes.cbegin();
-    auto pt_nodes_to_nodes = point_nodes_to_nodes.cbegin();
-    auto x_nodes = x.cbegin();
-    auto x_points = xp.begin();
-    auto gauss_pts = hpc::make_iterator_range(gauss_points.cbegin(), gauss_points.cend());
-    auto point_func = [=] HPC_DEVICE(const point_index point)
-    {
-      auto const point_nodes = pt_to_pt_nodes[point];
-      hpc::array<hpc::position<double>, 4> x;
-      assert(point_nodes.size() == 4);
-      for (int i = 0; i < 4; ++i)
-      {
-        auto const node = pt_nodes_to_nodes[point_nodes[i]];
-        x[i] = x_nodes[node].load();
-      }
-      for (auto gp : gauss_pts)
-      {
-        x_points[point] = tetrahedron_local_to_global_coord(x, gp);
-      }
-    };
-    hpc::for_each(hpc::device_policy(), points, point_func);
-  }
-
-  hpc::device_vector<hpc::position<double>, point_node_index> gauss_points;
-};
-
 HPC_ALWAYS_INLINE HPC_HOST_DEVICE hpc::position<double>
 tet_parametric_to_physical(hpc::array<hpc::position<double>, 4> const& xn,
     point_index const point, int const points_per_element)
