b5c725c5bee675b6318088d98b79447dd4b9549d
Julia Plews <japlews@sandia.gov>
2020-03-30 11:57:12 -0600

    Add more output to OTM VTK files

--- v3/otm_vtk.cpp
+++ v3/otm_vtk.cpp
@@ -21,22 +21,39 @@ void otm_file_writer::capture(state const& s) {
 
   hpc::copy(s.x, host_s.x);
   hpc::copy(s.xp, host_s.xp);
-
   hpc::copy(s.rho, host_s.rho);
   hpc::copy(s.V, host_s.V);
+
+  host_s.v.resize(num_nodes);
+  hpc::copy(s.v, host_s.v);
+  host_s.u.resize(num_nodes);
+  hpc::copy(s.u, host_s.u);
+  host_s.mass.resize(num_nodes);
+  hpc::copy(s.mass, host_s.mass);
+
+  host_s.F_total.resize(num_points);
+  hpc::copy(s.F_total, host_s.F_total);
+  host_s.sigma.resize(num_points);
+  hpc::copy(s.sigma_full, host_s.sigma);
+  host_s.G.resize(num_points);
+  hpc::copy(s.G, host_s.G);
+  host_s.K.resize(num_points);
+  hpc::copy(s.K, host_s.K);
 }
 
 void otm_file_writer::write(int const file_output_index) {
   auto node_stream = make_vtk_output_stream(prefix + "_nodes", file_output_index);
   auto point_stream = make_vtk_output_stream(prefix + "_points", file_output_index);
 
-  node_stream << std::scientific << std::setprecision(17);
   start_vtk_unstructured_grid_file(node_stream);
   //POINTS (nodes)
   write_vtk_points(node_stream, host_s.x);
   write_vtk_point_data(node_stream, host_s.nodes);
   assert(host_s.x.size() == host_s.nodes.size());
   write_vtk_vectors(node_stream, "node_position", host_s.x);
+  write_vtk_vectors(node_stream, "node_displacement", host_s.u);
+  write_vtk_vectors(node_stream, "node_velocity", host_s.v);
+  write_vtk_scalars(node_stream, "node_mass", host_s.mass);
   node_stream.close();
 
   start_vtk_unstructured_grid_file(point_stream);
@@ -47,6 +64,10 @@ void otm_file_writer::write(int const file_output_index) {
   write_vtk_vectors(point_stream, "point_position", host_s.xp);
   write_vtk_scalars(point_stream, "density", host_s.rho);
   write_vtk_scalars(point_stream, "volume", host_s.V);
+  write_vtk_full_tensors(point_stream, "sigma", host_s.sigma);
+  write_vtk_full_tensors(point_stream, "deformation_gradient", host_s.F_total);
+  write_vtk_scalars(point_stream, "G", host_s.G);
+  write_vtk_scalars(point_stream, "K", host_s.K);
   point_stream.close();
 }
 
--- v3/otm_vtk.hpp
+++ v3/otm_vtk.hpp
@@ -1,11 +1,29 @@
 #pragma once
 
+#include <hpc_array_vector.hpp>
+#include <hpc_dimensional.hpp>
+#include <hpc_range.hpp>
+#include <lgr_mesh_indices.hpp>
 #include <otm_host_pinned_state.hpp>
 #include <string>
 
 namespace lgr {
 
 class state;
+
+struct otm_host_pinned_output_state : public otm_host_pinned_state {
+  hpc::counting_range<node_index> nodes{node_index(0)};
+  hpc::counting_range<point_index> points{point_index(0)};
+
+  hpc::pinned_array_vector<hpc::displacement<double>, node_index> u;
+  hpc::pinned_array_vector<hpc::velocity<double>, node_index> v;
+  hpc::pinned_vector<hpc::mass<double>, node_index> mass;
+
+  hpc::pinned_array_vector<hpc::deformation_gradient<double>, point_index> F_total;
+  hpc::pinned_array_vector<hpc::stress<double>, point_index> sigma;
+  hpc::pinned_vector<hpc::pressure<double>, point_index> K;
+  hpc::pinned_vector<hpc::pressure<double>, point_index> G;
+};
 
 class otm_file_writer
 {
@@ -18,7 +36,7 @@ public:
   void capture(state const &s);
   void write(int const file_output_index);
 
-  otm_host_pinned_state host_s;
+  otm_host_pinned_output_state host_s;
 };
 
 }
