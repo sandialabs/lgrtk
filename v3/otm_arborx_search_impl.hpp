7a4c12ddd9cdaf5402b7f8872b28a8ba6d16c25d
Julia Plews <japlews@sandia.gov>
2020-04-15 11:26:33 -0600

    ArborX: use device_type instead of mem_space in Kokkos Views

--- v3/otm_arborx_search_impl.hpp
+++ v3/otm_arborx_search_impl.hpp
@@ -37,11 +37,12 @@ using device_mem_space = Kokkos::CudaSpace;
 #else
 using device_mem_space = device_exec_space::memory_space;
 #endif
-using device_point_view = Kokkos::View<ArborX::Point *, device_mem_space>;
-using device_sphere_view = Kokkos::View<ArborX::Sphere *, device_mem_space>;
-using device_nearest_query_view = Kokkos::View<ArborX::Nearest<ArborX::Point> *, device_mem_space>;
-using device_intersects_query_view = Kokkos::View<ArborX::Intersects<ArborX::Sphere> *, device_mem_space>;
-using device_int_view = Kokkos::View<int *, device_mem_space>;
+using device_type = Kokkos::Device<device_exec_space, device_mem_space>;
+using device_point_view = Kokkos::View<ArborX::Point *, device_type>;
+using device_sphere_view = Kokkos::View<ArborX::Sphere *, device_type>;
+using device_nearest_query_view = Kokkos::View<ArborX::Nearest<ArborX::Point> *, device_type>;
+using device_intersects_query_view = Kokkos::View<ArborX::Intersects<ArborX::Sphere> *, device_type>;
+using device_int_view = Kokkos::View<int *, device_type>;
 
 void initialize();
 
@@ -53,15 +54,15 @@ device_point_view create_arborx_points(const lgr::state &s);
 
 device_sphere_view create_arborx_point_spheres(const lgr::state &s);
 
-device_nearest_query_view make_nearest_node_queries(device_point_view points,
+device_nearest_query_view make_nearest_node_queries(const device_point_view& points,
     const int num_nodes_to_find);
 
-device_intersects_query_view make_intersect_sphere_queries(device_sphere_view point_spheres);
+device_intersects_query_view make_intersect_sphere_queries(const device_sphere_view& point_spheres);
 
 void inflate_sphere_query_radii(device_intersects_query_view queries, double factor);
 
 template<typename query_view_type>
-void do_search(device_point_view nodes, query_view_type queries, device_int_view &indices,
+void do_search(const device_point_view& nodes, const query_view_type& queries, device_int_view &indices,
     device_int_view &offsets);
 
 }
