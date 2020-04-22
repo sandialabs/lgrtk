3ba792c3e2da778eefaff6ade9558d29a8da3211
Alejandro Mota <amota@sandia.gov>
2020-04-22 14:42:19 -0700

    Add nu != 0 patch test
    Not truly accurate because of log strains, but
    serves as a guard against code breakeage.

--- otm.cpp
+++ otm.cpp
@@ -6,8 +6,8 @@
 int main()
 {
   HPC_TRAP_FPE();
-  if ((1)) lgr::otm_j2_nu_zero_patch_test();
-  if ((0)) lgr::otm_j2_uniaxial_patch_test();
+  if ((0)) lgr::otm_j2_nu_zero_patch_test();
+  if ((1)) lgr::otm_j2_uniaxial_patch_test();
   if ((0)) lgr::otm_cylindrical_flyer();
   return 0;
 }
