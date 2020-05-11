522f80a393005b723778723d96ae108321afbf53
Julia Plews <japlews@sandia.gov>
2020-05-11 16:48:01 -0600

    Miscellaneous fixes for GPU build and test:
      - Enable building with nvcc_wrapper for ease of linking in ArborX with
        correct Kokkos configuration.
      - Do not call Kokkos::initialize() and finalize() through search interface
        more than once in unit tests.
      - Create an object 'otm_scope' intended to protect against calling
        initialize()/finalize() more than once, and move search initialize and
        finalize calls out to main().
      - Fix function definition order to properly resolve auto types.

--- v3/otm.cpp
+++ v3/otm.cpp
@@ -6,6 +6,7 @@
 int main()
 {
   HPC_TRAP_FPE();
+  lgr::otm_scope scope;
   if ((0)) lgr::otm_j2_nu_zero_patch_test();
   if ((0)) lgr::otm_j2_uniaxial_patch_test();
   if ((1)) lgr::otm_cylindrical_flyer();
